5d49011a573db0abfc2f9cc68bf5f4a4865ef720
aleksei_rodimkin <alexeyrodimkin@gmail.com>
2025-11-10 04:20:11 +0300

    Profile added to Admin Dash

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = "pk", "bio", "user_verbose"
+    list_display_links = []
+    ordering = "pk",
+    search_fields = []
+
+    def get_queryset(self, request):
+        """Оптимизация выгрузки пользователей"""
+        return Profile.objects.select_related("user")
+
+    def user_verbose(self, obj: Profile) -> str:
+        """
+        Отображение пользователя в административной панели в поле Profile.
+        Отображать имя или username
+        """
+        return obj.user.first_name or obj.user.username
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -6,3 +6,6 @@ from django.db.models import CASCADE
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
     bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f"Profile #{self.pk} by {self.user.username}"
